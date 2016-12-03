613f0158586178cc51b48821db61e30a6cd1dfc5
Daniel Kovalevich <dkoval526@gmail.com>
2016-12-03 11:53:23 -0500

    Added Filter

---
+++ src/core/filter.java
@@ -0,0 +1,22 @@
+package core;
+
+import java.util.List;
+import twitter4j.Status;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class filter {
+	public List<String> createFile() {
+		List<String> output;
+		
+		
+		
+		return output;
+	}
+	
+	public void filterContent(List<Status> status){
+		List<String> output;
+		
+	}
+}
