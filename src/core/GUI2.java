83290bc5cdcb957f0d5e383654cf3e7b170f8063
Vilyamir Kolesnichenko <vilyk526@gmail.com>
2016-12-03 19:37:02 -0500

    Fixed GUI

--- src/core/GUI2.java
+++ src/core/GUI2.java
@@ -8,10 +8,14 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JSeparator;
 import java.awt.Canvas;
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+import java.awt.Component;
+import java.awt.Color;
+import java.awt.SystemColor;
 
 public class GUI2 {
 
-	private JFrame frame;
+	private JFrame frmVvf;
 	private JTextField txtTypeInA;
 
 	/**
@@ -22,7 +26,7 @@ public class GUI2 {
 			public void run() {
 				try {
 					GUI2 window = new GUI2();
-					window.frame.setVisible(true);
+					window.frmVvf.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -36,50 +40,59 @@ public class GUI2 {
 	public GUI2() {
 		initialize();
 	}
-
+	Color twitterBlue = new Color (0, 172, 237);
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmVvf = new JFrame();
+		frmVvf.getContentPane().setBackground(UIManager.getColor(twitterBlue));
+		frmVvf.setOpacity(100.0f);
+		frmVvf.setBackground(UIManager.getColor("Table.selectionBackground"));
+		frmVvf.setTitle("Twitter Bot");
+		frmVvf.setForeground(Color.ORANGE);
+		frmVvf.setBounds(100, 100, 801, 518);
+		frmVvf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmVvf.getContentPane().setLayout(null);
 		
 		JTextPane txtpnToCheckWeather = new JTextPane();
-		txtpnToCheckWeather.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		txtpnToCheckWeather.setBackground(UIManager.getColor(0, 172, 237));
+		txtpnToCheckWeather.setForeground(SystemColor.info);
+		txtpnToCheckWeather.setFont(new Font("Tahoma", Font.PLAIN, 26));
+		txtpnToCheckWeather.setBackground(UIManager.getColor(twitterBlue));
 		txtpnToCheckWeather.setText("To check weather conditions within a region, type in a state.");
-		txtpnToCheckWeather.setBounds(10, 11, 349, 34);
-		frame.getContentPane().add(txtpnToCheckWeather);
+		txtpnToCheckWeather.setBounds(10, 11, 685, 70);
+		frmVvf.getContentPane().add(txtpnToCheckWeather);
 		
 		txtTypeInA = new JTextField();
 		txtTypeInA.setForeground(UIManager.getColor("Button.shadow"));
-		txtTypeInA.setFont(new Font("Tahoma", Font.ITALIC, 10));
+		txtTypeInA.setFont(new Font("Tahoma", Font.ITALIC, 23));
 		txtTypeInA.setText("Type in a State");
-		txtTypeInA.setBounds(10, 56, 86, 20);
-		frame.getContentPane().add(txtTypeInA);
+		txtTypeInA.setBounds(10, 91, 315, 43);
+		frmVvf.getContentPane().add(txtTypeInA);
 		txtTypeInA.setColumns(10);
 		
 		JButton btnSearch = new JButton("Search");
-		btnSearch.setBounds(121, 54, 89, 23);
-		frame.getContentPane().add(btnSearch);
+		btnSearch.setFont(new Font("Tahoma", Font.PLAIN, 26));
+		btnSearch.setBounds(401, 91, 206, 43);
+		frmVvf.getContentPane().add(btnSearch);
 		
 		JSeparator separator = new JSeparator();
-		separator.setBounds(10, 87, 414, 2);
-		frame.getContentPane().add(separator);
+		separator.setBounds(10, 145, 765, 2);
+		frmVvf.getContentPane().add(separator);
 		
 		JTextPane txtpnRegionsAndConditions = new JTextPane();
-		txtpnRegionsAndConditions.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		txtpnRegionsAndConditions.setBackground(UIManager.getColor(0));
+		txtpnRegionsAndConditions.setForeground(SystemColor.info);
+		txtpnRegionsAndConditions.setFont(new Font("Tahoma", Font.PLAIN, 26));
+		txtpnRegionsAndConditions.setBackground(UIManager.getColor(twitterBlue));
 		txtpnRegionsAndConditions.setText("Regions and Conditions");
-		txtpnRegionsAndConditions.setBounds(10, 100, 215, 20);
-		frame.getContentPane().add(txtpnRegionsAndConditions);
+		txtpnRegionsAndConditions.setBounds(10, 156, 414, 43);
+		frmVvf.getContentPane().add(txtpnRegionsAndConditions);
 		
 		Canvas canvas = new Canvas();
-		canvas.setBackground(UIManager.getColor("controlHighlight"));
-		canvas.setBounds(10, 126, 414, 125);
-		frame.getContentPane().add(canvas);
+		canvas.setBackground(Color.WHITE);
+		canvas.setBounds(10, 219, 765, 250);
+		frmVvf.getContentPane().add(canvas);
+		frmVvf.setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{frmVvf.getContentPane(), txtpnToCheckWeather, txtTypeInA, btnSearch, separator, txtpnRegionsAndConditions, canvas}));
 	}
 
 }
